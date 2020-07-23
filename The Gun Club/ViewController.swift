57be7adca8440451cde09a874ccc5f80bbd2bf62
Chuck Zumbaugh <zumbaughca@gmail.com>
2020-07-23 15:44:23 -0400

    Initial Commit

---
+++ The Gun Club/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Gun Club
+//
+//  Created by Chuck Zumbaugh on 7/23/20.
+//  Copyright © 2020 Chuck Zumbaugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
